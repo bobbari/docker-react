f0ad75e0c87fa0df8f8dee425dc8833ec0fd7d6d
mahendra <mahendranath.bobbari@gmail.com>
2023-03-16 22:50:56 +0530

    added travis.yml

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hi Mahendra nath/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link new case', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hi Mahendra nath/i);
   expect(linkElement).toBeInTheDocument();
 });
